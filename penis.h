6e69333a3f2ce65cebd40242aa321a650b572d5f
djsdoodle <46250258+djsdoodle@users.noreply.github.com>
2021-11-24 00:00:44 -0500

    Add files via upload

--- penis.h
+++ penis.h
@@ -1,6 +1,6 @@
 --obamas penis stretcher
 
-global function git_commit_suicide(x:number, y:number, z:string)
+global function printx(x:number, y:number, z:string)
 	for i = x, y do
 		print(z)
 	end
